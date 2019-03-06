b4d6d9ff96c52e398f907c22ad11104ef64d0363
ross <dev@rross.me>
2019-03-06 15:15:28 -0600

    Comment generation and addition to database complete. Responses yet to fulfill requirements.

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 let database = {
   users: {},
   articles: {},
-  nextArticleId: 1
+  nextArticleId: 1,
+  comments: {},
+  nextCommentId: 1
 };
 
 const routes = {
@@ -26,8 +28,46 @@ const routes = {
   },
   '/articles/:id/downvote': {
     'PUT': downvoteArticle
-  }
+  },
+  '/comments': {
+    'POST': createComment
+  },
+  '/comments/:id': {},
+  '/comments/:id/upvote': {},
+  '/comments/:id/downvote': {}
 };
+
+/* Methods by Ross */
+function createComment(url, request) {
+  const requestComment = request.body && request.body.comment;
+  const response = {};
+
+  if(requestComment){
+    const comment = {
+      id: database.nextCommentId,
+      body: requestComment.body,
+      username: requestComment.username,
+      articleId: requestComment.articleId,
+      upvotedBy: [],
+      downvotedBy: []
+    }
+
+    database.comments[comment.id] = comment;
+    console.log(database);
+    database.users[comment.username].commentIds.push(comment.id);
+    database.articles[comment.articleId].commentIds.push(comment.id);
+
+    database.nextCommentId++;
+
+    response.body = comment;
+    response.status = 201;
+  } else {
+    response.status = 400;
+  }
+
+  return response;
+}
+/* End methods by Ross */
 
 function getUser(url, request) {
   const username = url.split('/').filter(segment => segment)[1];
